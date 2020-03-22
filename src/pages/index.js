ae9c4b0a20a62030eee1e38a73cc1f78a9b75f5d
mikesinc <mas.sinclair@gmail.com>
2020-03-23 00:25:37 +0800

    Initial Gatsby Upload

---
+++ src/pages/index.js
@@ -0,0 +1,216 @@
+import React from "react"
+import Container from "react-bootstrap/Container"
+import Navbar from "../Components/Navbar"
+import ContactForm from "../Components/ContactForm"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { graphql } from "gatsby"
+import SEO from "../Components/seo"
+
+export default ({ data }) => {
+  return (
+    <>
+      <Navbar />
+      <SEO title={data.contentfulTitle.title} />
+      <Container fluid className="overall">
+        <Container fluid className="banner">
+          <video className="backgroundVideo" loop autoPlay muted>
+            <source
+              src={data.contentfulTitle.bannerVideo.file.url}
+              type="video/mp4"
+            />
+            Your browser does not support the video.
+          </video>
+
+          <Container
+            fluid
+            className="glitch"
+            data-text={data.contentfulTitle.title}
+          >
+            {data.contentfulTitle.title}
+          </Container>
+        </Container>
+
+        <Container
+          fluid
+          className="about"
+          style={{
+            backgroundImage: `url(${data.contentfulTitle.tonyImage.file.url}), linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0), rgba(0,0,0,1))`,
+          }}
+        >
+          <p>{data.contentfulTitle.blurb.blurb}</p>
+        </Container>
+
+        <Container fluid className="portfolio">
+          <iframe
+            className="portfolioVideo"
+            title="Oh Gosh & Boomtick Presents; AJ Tracy"
+            src={data.contentfulTitle.preivewVideoLink}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <p>
+            Please check out my{" "}
+            <a
+              href="https://vimeo.com/tonyknight"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "yellow" }}
+            >
+              Vimeo
+            </a> for my portfolio
+          </p>
+        </Container>
+
+        <Container
+          fluid
+          className="fightweek"
+          style={{
+            backgroundImage: `url(${data.contentfulTitle.background2.file.url}), linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0), rgba(0,0,0,1))`,
+          }}
+        >
+          <h1>{data.contentfulTitle.fightweekTitle}</h1>
+          <p>{data.contentfulTitle.fightweekBlurb}</p>
+          <p>Link to the series YouTube below</p>
+          <iframe
+            className="fightVideo"
+            title="FIGHTWEEK EPISODE 1: Chris Killa Watt Pt1"
+            src={data.contentfulTitle.fightWeekVideoLink}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </Container>
+
+        <Container fluid className="contact" style={{
+            backgroundImage: `url(${data.contentfulTitle.background3.file.url}), linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0), rgba(0,0,0,1))`,
+          }}>
+          <h1>{data.contentfulTitle.contactTitle}</h1>
+          <p>{data.contentfulTitle.contactHeader}</p>
+          <ContactForm />
+          <Container fluid className="author">
+          <Container fluid className="links">
+            <a
+              href="https://www.facebook.com/slimjongil/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("../assets/images/facebook.png")}
+                height="30px"
+                width="30px"
+                alt="slimjongil_fb"
+              ></img>
+            </a>
+            <a
+              href="https://www.instagram.com/slimjongil/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("../assets/images/instagram.png")}
+                height="30px"
+                width="30px"
+                alt="slimjongil_ig"
+              ></img>
+            </a>
+            <a
+              href="https://vimeo.com/tonyknight"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("../assets/images/vimeo.png")}
+                height="30px"
+                width="30px"
+                alt="slimjongil_vm"
+              ></img>
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCmXvxk9rqozVQU6RE1EMdYw"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("../assets/images/youtube.png")}
+                height="30px"
+                width="30px"
+                alt="slimjongil_yt"
+              ></img>
+            </a>
+          </Container>
+            <div>
+              <a
+                href="https://github.com/mikesinc"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                website by mikesinc
+              </a>
+              <div style={{ fontSize: "6px" }}>
+                Icons made by{" "}
+                <a
+                  href="https://www.flaticon.com/authors/freepik"
+                  title="Freepik"
+                >
+                  Freepik
+                </a>{" "}
+                from{" "}
+                <a href="https://www.flaticon.com/" title="Flaticon">
+                  www.flaticon.com
+                </a>
+              </div>
+            </div>
+          </Container>
+        </Container>
+      </Container>
+    </>
+  )
+}
+
+export const query = graphql`
+  {
+    contentfulTitle {
+      title
+      blurb {
+        blurb
+      }
+      bannerVideo {
+        file {
+          url
+        }
+      }
+      background3 {
+        file {
+          url
+        }
+      }
+      background2 {
+        file {
+          url
+        }
+      }
+      background1 {
+        file {
+          url
+        }
+      }
+      tonyImage {
+        file {
+          url
+        }
+      }
+      contactHeader
+      contactTitle
+      fightWeekVideoLink
+      fightweekBlurb
+      fightweekTitle
+      navbarLogo {
+        file {
+          url
+        }
+      }
+      preivewVideoLink
+    }
+  }
+`

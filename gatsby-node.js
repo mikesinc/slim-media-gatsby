5ee972d133dbeed7d0e2cc649c3131311c3bf678
mikesinc <mas.sinclair@gmail.com>
2020-03-24 21:00:49 +0800

    redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.onCreateNode = ({ actions }) => {
   const { createRedirect } = actions
   createRedirect({
     fromPath: "https://slim-media.netlify.com/*",
-    toPath: "/https://www.slim-media.com/:splat",
+    toPath: "https://www.slim-media.com/:splat",
     isPermanent: true,
   })
 }

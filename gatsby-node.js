64c89e15a33743f78058bfec64459e844a3e307b
mikesinc <mas.sinclair@gmail.com>
2020-03-24 21:14:55 +0800

    redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,5 +4,6 @@ exports.createPages = ({ actions }) => {
     fromPath: "https://slim-media.netlify.com/*",
     toPath: "https://www.slim-media.com/:splat",
     isPermanent: true,
+    force: true
   })
 }
